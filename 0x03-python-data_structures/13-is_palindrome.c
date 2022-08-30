cb73cd62065d84d587fb28f694e7018b76982023
Bratipah <bratipahmh@gmail.com>
2022-08-29 18:30:20 -0700

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,19 +1,32 @@
-# include <Python.h>
-# include <object.h>
-# include <listobject.h>
+#include "lists.h"
 
-void print_python_list_info(PyObject * p)
+/**
+ * palindrom -  recursive palind or not
+ * @head: head list
+ * Return: 0 if it is not a palindrome
+ * 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
 {
-    long int size = PyList_Size(p)
-    int i
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (aux_palind(head, *head));
+}
 
-    PyListObject * obj = (PyListObject * )p
-
-    printf(" [ * ] Size of the Python List = %li\n", size)
-
-    printf(" [ * ] Allocated = %li\n", obj -> allocated)
-
-    for (i=0, i < size, i++)
-
-    printf("Element %i: %s\n", i, Py_TYPE(obj -> ob_item[i]) -> tp_name)
+/**
+ * aux_palind - funct to know if is palindrome
+ * @head: head list
+ * @end: end list
+ */
+int aux_palind(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+		return (1);
+	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
