60a78984136d55127ec7f7398c4b84a68c7925ea
Bratipah <bratipahmh@gmail.com>
2022-08-31 15:24:15 -0700

    numbers

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-def print_sorted_dictionary(a_dictionary):
+def number_keys(a_dictionary):
 
-    for i in sorted(a_dictionary):
-
-        print("{:s}: {}".format(i, a_dictionary[i]))
+        return len(a_dictionary.keys())
