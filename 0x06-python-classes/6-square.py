3024e18089ee03c7e310aec7d5d5eccac3e3b44a
Bratipah <bratipahmh@gmail.com>
2022-09-14 15:01:47 -0700

    add files

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -2,6 +2,7 @@
 
 """ Creating a square class """
 
+
 class Square:
 
     """
@@ -9,29 +10,21 @@ class Square:
 
     """
 
-
     def __init__(self, size=0, position=(0, 0)):
-
         """Initializes the data."""
 
         self.size = size
 
         self.position = position
 
-
     @property
-
     def size(self):
-
         """Returns the size of the square."""
 
         return self.__size
 
-
     @size.setter
-
     def size(self, value):
-
         """Sets the size to a value."""
 
         if not isinstance(value, int):
@@ -44,52 +37,36 @@ class Square:
 
         self.__size = value
 
-
     @property
-
     def position(self):
-
         """Retrieves the position."""
-
         return self.__position
 
-
     @position.setter
-
     def position(self, value):
-
         """Sets the position to a value."""
 
         if not isinstance(value, tuple) or len(value) != 2:
-
             raise TypeError("position must be a tuple of 2 positive integers")
 
         if not isinstance(value[0], int) or not isinstance(value[1], int):
-
             raise TypeError("position must be a tuple of 2 positive integers")
 
         if value[0] < 0 or value[1] < 0:
-
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
 
-
     def area(self):
-
         """Returns the current square area."""
-
         return self.__size ** 2
 
-
     def my_print(self):
-
         """Prints to stdout the square with the character #,
         at the position given by the position attribute.
         """
 
         if self.__size == 0:
-
             print()
 
             return
