13cec1e024907d7e148542260fb0adcd4dbb8d4b
Bratipah <bratipahmh@gmail.com>
2022-09-14 15:12:04 -0700

    dd class files

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -5,8 +5,7 @@
 
 class Node:
 
-
-    """Defining Node linked list."""
+    """Defining node linked list."""
 
     def __init__(self, data, next_node=None):
 
@@ -25,55 +24,42 @@ class Node:
         self.next_node = next_node
 
     @property
-
     def data(self):
-
         """Get/set the data of the Node."""
 
         return (self.__data)
 
     @data.setter
-
     def data(self, value):
-
         if not isinstance(value, int):
-
             raise TypeError("data must be an integer")
 
         self.__data = value
 
-
     @property
-
     def next_node(self):
-
         """Get/set the next_node of the Node."""
 
         return (self.__next_node)
 
     @next_node.setter
-
     def next_node(self, value):
-
         if not isinstance(value, Node) and value is not None:
-
             raise TypeError("next_node must be a Node object")
 
         self.__next_node = value
 
+
 class SinglyLinkedList:
+
     """Represent a singly-linked list."""
 
-
     def __init__(self):
-
         """Initalize a new SinglyLinkedList."""
 
         self.__head = None
 
-
     def sorted_insert(self, value):
-
         """Insert a new Node to the SinglyLinkedList.
 
         The node is inserted into the list at the correct
@@ -88,7 +74,6 @@ class SinglyLinkedList:
         new = Node(value)
 
         if self.__head is None:
-
             new.next_node = None
 
             self.__head = new
@@ -113,9 +98,7 @@ class SinglyLinkedList:
 
             tmp.next_node = new
 
-
     def __str__(self):
-
         """Define the print() representation of a SinglyLinkedList."""
 
         values = []
