037b17cf8345980f69035d8ec25e514f7692718f
Bratipah <bratipahmh@gmail.com>
2022-09-14 15:25:52 -0700

    add square file

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -3,34 +3,27 @@
 """ Creating a square class """
 
 
-
-
 class Square
 
-    """ Defining a class square """
 
-    def __init__(self, size=0):
+""" Defining a class square """
 
-        """ Initializing a square class
+
+def __init__(self, size=0):
+    """ Initializing a square class
         Args: size=0: size of the square
          """
 
-        self.__size = size
-
-
+    self.__size = size
 
     @property
-
     def size(self):
 
         """ Getting the size of the square """
 
         return self.__size
 
-
-
     @size.setter
-
     def size(self, value):
 
         """ Setting the size of the square """
@@ -45,15 +38,11 @@ class Square
 
         self.__size = value
 
-
-
     def area(self):
 
         """ Calculating the area of the square """
 
         return (self.__size ** 2)
-
-
 
     def __eq__(self, other):
 
@@ -61,15 +50,11 @@ class Square
 
         return self.area() == other.area()
 
-
-
     def __ne__(self, other):
 
         """ Comparing != two squares """
 
         return self.area() != other.area()
-
-
 
     def __lt__(self, other):
 
@@ -77,23 +62,17 @@ class Square
 
         return self.area() < other.area()
 
-
-
     def __le__(self, other):
 
         """ Comparing <= two squares """
 
         return self.area() <= other.area()
 
-
-
     def __gt__(self, other):
 
         """ Comparing > two squares """
 
         return self.area() > other.area()
-
-
 
     def __ge__(self, other):
 
